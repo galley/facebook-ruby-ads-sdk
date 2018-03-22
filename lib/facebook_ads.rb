260de9483f1717806f77582bbf53c59dc18a2d88
Ian Costello <ian@galleyfoods.com>
2018-03-22 13:53:08 -0400

    change logger to STDOUT

--- lib/facebook_ads.rb
+++ lib/facebook_ads.rb
@@ -16,7 +16,7 @@ module FacebookAds
 
   def self.logger
     unless defined?(@logger)
-      @logger       = Logger.new('/dev/null')
+      @logger       = Logger.new(STDOUT)
       @logger.level = Logger::Severity::UNKNOWN
     end
 
